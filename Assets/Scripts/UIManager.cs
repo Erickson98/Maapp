63a16f5a4624077c40476b3053da8cf84c8e96e0
WanderTheOrangeAlien <01miguelurena@gmail.com>
2023-06-15 18:35:21 -0300

    UI Manager - All Screens

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,19 +6,55 @@ using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-    public UniversitySelect[] SeleccionUniversidades = new UniversitySelect[4];
+    //public UniversitySelect[] SeleccionUniversidades = new UniversitySelect[4];
+    public ButtonPagePair[] SelectionPage_Routes = new ButtonPagePair[4]; //For going from select page to routes
+    public ButtonPagePair[] SelectionPage_Info = new ButtonPagePair[4]; //From select to info
+
+    public Button[] InfoPage_Return = new Button[4];
+    public ButtonPagePair[] InfoPage_Routes = new ButtonPagePair[4];
+
     public GameObject selectionPage;
     
     void Start()
     {
         changeToPage(selectionPage); //CHANGE: This is the first page to show to the user
 
-        for (int i = 0; i < SeleccionUniversidades.Length; i++)
+        //Connect each button with its target page
+
+        for (int i = 0; i < SelectionPage_Routes.Length; i++)
         {
             int index = i; //Local copy of i is needed becasue the lambda function captures the reference of i instead of its value.
-            SeleccionUniversidades[index].button.onClick.AddListener(() =>
+            SelectionPage_Routes[index].button.onClick.AddListener(() =>
             {
-                changeToPage(SeleccionUniversidades[index].targetPage);
+                changeToPage(SelectionPage_Routes[index].targetPage);
+            });
+        }
+
+        for (int i = 0; i < SelectionPage_Info.Length; i++)
+        {
+            int index = i; 
+            SelectionPage_Info[index].button.onClick.AddListener(() =>
+            {
+                changeToPage(SelectionPage_Info[index].targetPage);
+            });
+        }
+
+        //All the return buttons on the info pages always go to Selection Page
+        for (int i = 0; i < InfoPage_Return.Length; i++)
+        {
+            int index = i;
+            InfoPage_Return[index].onClick.AddListener(() =>
+            {
+                changeToPage(selectionPage);
+            });
+        }
+
+        for (int i = 0; i < InfoPage_Routes.Length; i++)
+        {
+            int index = i;
+            InfoPage_Routes[index].button.onClick.AddListener(() =>
+            {
+                changeToPage(InfoPage_Routes[index].targetPage);
             });
         }
     }
@@ -36,11 +72,13 @@ public class UIManager : MonoBehaviour
     {
         //Deactivate all pages
 
-        //This for loop may be changed
-        for (int i = 0; i < SeleccionUniversidades.Length; i++)
+        foreach(Transform child in transform)
         {
-            SeleccionUniversidades[i].targetPage.SetActive(false);
+            child.gameObject.SetActive(false);
         }
+
+        //This for loop may be changed
+        
         //Deactivate the other pages
 
 
@@ -52,6 +90,22 @@ public class UIManager : MonoBehaviour
 }
 [System.Serializable]
 public struct UniversitySelect
+{
+    public Button button;
+    public GameObject targetPage;
+    public Button infoButton;
+    public GameObject infoPage;
+}
+
+[System.Serializable]
+public struct InfoPage
+{
+    public Button returnToSelectionBtn;
+    public Button ViewRouteBtn;
+}
+
+[System.Serializable]
+public struct ButtonPagePair
 {
     public Button button;
     public GameObject targetPage;
